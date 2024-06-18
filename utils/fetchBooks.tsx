959a6779055984aefb25aa5c1e39aeabd67dec6c
Andrew <ondralukes06@seznam.cz>
2024-06-18 20:09:42 +0200

    added enviroment variables

--- utils/fetchBooks.tsx
+++ utils/fetchBooks.tsx
@@ -1,9 +1,15 @@
 import axios from 'axios';
 import { Book } from '@/types/types';
 
+// const apiUrl =  process.env.LOCAL_APP_API_URL;
+
+
 export const getBooksByQuery = async (query = ''): Promise<Book[]> => {
+
+  const apiUrl = process.env.NEXT_PUBLIC_APP_API_URL;
+  console.log(apiUrl)
   try {
-    const response = await axios.get("http://localhost:3002/bookList", { params: {   query} });
+    const response = await axios.get(`${apiUrl}/bookList`, { params: {   query} });
     const data = response.data;
 
     if (data === null) {
