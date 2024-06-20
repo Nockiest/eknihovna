d5d087e6ca6c296e0364513b1ac5661fb97fb500
Andrew <ondralukes06@seznam.cz>
2024-06-20 14:01:56 +0200

    moved data to server dir

--- dist/data.js
+++ dist/data.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.testURL = exports.knihyURL = void 0;
-exports.knihyURL = 'C:/Users/ondra/OneDrive - Gymnázium Opatov, Praha 4, Konstantinova 1500/Plocha/eknihovna/bookData/knihy.xlsx';
+exports.knihyURL = 'server/knihy.xlsx';
 exports.testURL = 'C:/Users/ondra/OneDrive - Gymnázium Opatov, Praha 4, Konstantinova 1500/Plocha/eknihovna/bookData/test.xlsx';
