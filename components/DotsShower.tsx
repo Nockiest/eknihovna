94907344b4a4dbc2711d04aa8924189661d7b11f
Andrew <ondralukes06@seznam.cz>
2024-06-21 09:15:49 +0200

    reworked loading screen

---
+++ components/DotsShower.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const DotsShower = () => {
+  const [dots, setDots] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prevDots) => (prevDots.length === 3 ? "" : prevDots + "."));
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <div
+    
+    >
+      <h1 style={{ fontSize: "24px" }}>Loading{dots}</h1>
+    </div>
+  );
+};
+
+export default DotsShower;
