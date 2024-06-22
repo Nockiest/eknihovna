453969c26693544f0445bd518108628bd792688d
Andrew <ondralukes06@seznam.cz>
2024-06-22 20:20:37 +0200

    added home page description

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,50 @@ import { Typography } from "@mui/material";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div>
-          <Typography  variant="h1" component="h2"> jumping over the lazy brown foxes</Typography>
-          <Typography  variant="h2" component="h2"> jumping over the lazy brown foxes</Typography>
-          <Typography  variant="h3" component="h2"> jumping over the lazy brown foxes</Typography>
-          <Typography  variant="h4" component="h2"> jumping over the lazy brown foxes</Typography>
-          <Typography  variant="h5" component="h2"> jumping over the lazy brown foxes</Typography>
-          <Typography  variant="h6" component="h2"> jumping over the lazy brown foxes</Typography>
-          <Typography  variant="body1" component="h2"> jumping over the lazy brown foxes</Typography>
-        </div>
+      <Typography variant="body1">
+        Vítejte na domovské stránce knihovny Gymnázia Opatov, kde se můžete
+        seznámit s bohatým fondem naší školní knihovny a získat důležité
+        informace. Naše knihovna je otevřeným prostorem pro vaše vzdělání a
+        inspiraci. Nabízíme široký výběr literatury, studijních materiálů a
+        časopisů. Projd2te si naši
+        sbírku a objevujte nové příběhy, poznatky a světy. Jsme tu pro vás,
+        abychom vám pomohli s vašimi studijními potřebami a abychom vám poskytli
+        prostor k odpočinku a pohodlnému čtení. Pokud máte zájem o konkrétní
+        knihu, nebo potřebujete poradit s vyhledáním informací, neváhejte se po školních hodinách
+        obrátit na profesorku Bauerovou. Náš cíl je podporovat váš akademický
+        růst a zvědavost. Přijďte navštívit naši knihovnu a objevte, co všechno
+        pro vás máme připraveného. Těšíme se na vaši návštěvu!
+      </Typography>
+      <div>
+        <Typography variant="h1" component="h2">
+          {" "}
+          jumping over the lazy brown foxes
+        </Typography>
+        <Typography variant="h2" component="h2">
+          {" "}
+          jumping over the lazy brown foxes
+        </Typography>
+        <Typography variant="h3" component="h2">
+          {" "}
+          jumping over the lazy brown foxes
+        </Typography>
+        <Typography variant="h4" component="h2">
+          {" "}
+          jumping over the lazy brown foxes
+        </Typography>
+        <Typography variant="h5" component="h2">
+          {" "}
+          jumping over the lazy brown foxes
+        </Typography>
+        <Typography variant="h6" component="h2">
+          {" "}
+          jumping over the lazy brown foxes
+        </Typography>
+        <Typography variant="body1" component="h2">
+          {" "}
+          jumping over the lazy brown foxes
+        </Typography>
+      </div>
     </main>
   );
 }
