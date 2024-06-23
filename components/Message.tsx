0430d877d1820958d9947bb6d1d504ae2e8019ac
Andrew <ondralukes06@seznam.cz>
2024-06-23 15:13:07 +0200

    added message component

---
+++ components/Message.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Box, Typography, Paper } from '@mui/material';
+
+interface MessageProps {
+  message: string;
+  date: string;
+}
+
+const Message: React.FC<MessageProps> = ({ message, date }) => {
+  return (
+    <Paper elevation={3} sx={{ p: 2, mb: 2, display: 'flex', flexDirection: 'column' }}>
+      <Typography variant="body1" gutterBottom>
+        {message}
+      </Typography>
+      <Typography variant="caption" color="textSecondary" align="right">
+        {date}
+      </Typography>
+    </Paper>
+  );
+};
+
+export default Message;
