4a39d88124f03a676d42c8dc0697eccf9006665b
Andrew <ondralukes06@seznam.cz>
2024-06-25 16:16:10 +0200

    added more elements to searcher

--- app/katalog/page.tsx
+++ app/katalog/page.tsx
@@ -37,7 +37,7 @@ const CatalogPage = () => {
       </Box>
       <BookCatalog promisedBooks={books} />
       {isOpenSearcher.toString()}
-      <Searcher isOpen={isOpenSearcher} />
+      <Searcher isOpen={isOpenSearcher} setIsOpen={setOpenSearcher} />
     </Box>
   );
 };
