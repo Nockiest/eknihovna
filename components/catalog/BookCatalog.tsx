9b58ec7b3ff1970bdbf137953a3143d803f0f85f
Andrew <ondralukes06@seznam.cz>
2024-07-02 15:19:12 +0200

    fetching data using filters

--- components/catalog/BookCatalog.tsx
+++ components/catalog/BookCatalog.tsx
@@ -1,25 +1,19 @@
-'use client'
+"use client";
 import React, { useState, useEffect } from "react";
-import {
-  Box,
-  Grid,
-  Typography,
-  useTheme,
-  Breakpoint,
-} from "@mui/material";
+import { Box, Grid, Typography, useTheme, Breakpoint } from "@mui/material";
 import { Book } from "@/types/types";
 import BookPreview from "./BookPreview";
 import DotsShower from "../general/DotsShower";
 import PaginationLinker from "../general/PaginationLinker";
 import { getURLSegment } from "@/utils/getURLSegment";
 import useCurrentBreakpoint from "@/utils/useCustomBreakpoint";
-import { usePathname ,useRouter, useSearchParams} from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import CategoryChip from "./CategoryChip";
 import { useSearchContext } from "@/app/katalog/context";
+import { PrimaryButton } from "@/theme/buttons/Buttons";
+import { getBooksByQuery } from "@/utils/fetchBooks";
 
-interface BookCatalogProps {
-  promisedBooks: Promise<Book[]> | Book[];
-}
+interface BookCatalogProps {}
 
 const shownBooksBySize: Record<Breakpoint, number> = {
   xs: 12,
@@ -29,11 +23,11 @@ const shownBooksBySize: Record<Breakpoint, number> = {
   xl: 36,
 };
 
-const BookCatalog: React.FC<BookCatalogProps> = ({ promisedBooks }) => {
+const BookCatalog: React.FC<BookCatalogProps> = () => {
   const theme = useTheme();
   const size = useCurrentBreakpoint();
   const router = useRouter();
-  const {filters,setFilters} = useSearchContext();
+  const { books, setBooks, filters, setFilters } = useSearchContext();
 
   const [allBooks, setAllBooks] = useState<Book[]>([]);
   const [error, setError] = useState<string | null>(null);
@@ -41,48 +35,65 @@ const BookCatalog: React.FC<BookCatalogProps> = ({ promisedBooks }) => {
   const [totalPages, setTotalPages] = useState<number>(0);
   const [itemsPerPage, setItemsPerPage] = useState<number>(12);
 
-  const searchParams = useSearchParams()
-  const page = parseInt(searchParams.get('page')||'1', 10)
+  const searchParams = useSearchParams();
+  const page = parseInt(searchParams.get("page") || "1", 10);
 
-  const setNewBookSlice = (page:number,itemsPerPage:number, allBooks:Book[]|undefined) => {
+  const setNewBookSlice = (
+    page: number,
+    itemsPerPage: number,
+    allBooks: Book[] | undefined
+  ) => {
     const indexOfFirstBook = page * itemsPerPage - itemsPerPage;
-    const newLastBookIndex = page * itemsPerPage
-    console.log(allBooks,allBooks===undefined )
-    setShownBooks(allBooks===undefined?[] : allBooks?.slice(indexOfFirstBook, newLastBookIndex) );
-  }
+    const newLastBookIndex = page * itemsPerPage;
+    console.log(allBooks, allBooks === undefined);
+    setShownBooks(
+      allBooks === undefined
+        ? []
+        : allBooks?.slice(indexOfFirstBook, newLastBookIndex)
+    );
+  };
   useEffect(() => {
     if (size) {
       const indexOfFirstBook = page * itemsPerPage - itemsPerPage;
       const newItemsPerPage = shownBooksBySize[size];
       setItemsPerPage(newItemsPerPage);
-      const newCurrentPage = Math.ceil((indexOfFirstBook) / newItemsPerPage);
-      setNewBookSlice(page, itemsPerPage, allBooks)
+      const newCurrentPage = Math.ceil(indexOfFirstBook / newItemsPerPage);
+      setNewBookSlice(page, itemsPerPage, allBooks);
       setTotalPages((prev) => {
-        return allBooks === undefined ? 0 : Math.ceil(allBooks.length / itemsPerPage);
+        return allBooks === undefined
+          ? 0
+          : Math.ceil(allBooks.length / itemsPerPage);
       });
-      router.push(`/katalog?page=${Math.min(Math.ceil(allBooks.length / itemsPerPage),newCurrentPage + 1||1)}`);
+      router.push(
+        `/katalog?page=${Math.min(
+          Math.ceil(allBooks.length / itemsPerPage),
+          newCurrentPage + 1 || 1
+        )}`
+      );
     }
-  }, [size,allBooks,itemsPerPage,  page]);
+  }, [size, allBooks, itemsPerPage, page]);
 
   // Fetch books on initial render
   useEffect(() => {
     router.push(`/katalog?page=1`); // reset page number on every query
     const fetchBooks = async () => {
       try {
-        const resolvedBooks = await promisedBooks;
+        const resolvedBooks = await books;
         setAllBooks(resolvedBooks);
       } catch (err: any) {
         setError(err.message || "Failed to fetch books");
       }
     };
     fetchBooks();
-  }, [promisedBooks ]);
+  }, [books]);
 
   // Update shown books based on currentPage and itemsPerPage
   useEffect(() => {
-    setNewBookSlice(page, itemsPerPage, allBooks)
+    setNewBookSlice(page, itemsPerPage, allBooks);
     setTotalPages((prev) => {
-      return allBooks === undefined ? 0 : Math.ceil(allBooks.length / itemsPerPage);
+      return allBooks === undefined
+        ? 0
+        : Math.ceil(allBooks.length / itemsPerPage);
     });
   }, [page, itemsPerPage, allBooks]);
 
@@ -98,17 +109,30 @@ const BookCatalog: React.FC<BookCatalogProps> = ({ promisedBooks }) => {
   const removeFilter = (key: string) => {
     console.log(filters);
     setFilters((prevFilters) => {
-        const { [key]: removedKey, ...rest } = prevFilters;
-        return rest;
+      const { [key]: removedKey, ...rest } = prevFilters;
+      return rest;
     });
-}
-
+  };
 
   return (
     <Box className="w-full">
-      {Object.entries(filters).map(([key, value], index) => (
-  <CategoryChip key={index} text={key} onRemove={() => removeFilter(key)} />
-))}
+      <Box>
+        {Object.entries(filters).map(([key, value], index) => (
+          <CategoryChip
+            key={index}
+            text={key}
+            onRemove={() => removeFilter(key)}
+          />
+        ))}
+        <PrimaryButton
+          onClick={async () => {
+            const newBooks = await getBooksByQuery(filters);
+            setBooks(newBooks);
+          }}
+        >
+          Použít Filtry
+        </PrimaryButton>
+      </Box>
 
       {shownBooks.length > 0 ? (
         <div className="w-full">
