fade904592e56c29cce225bc9f38c7eb0bf269f8
Andrew <ondralukes06@seznam.cz>
2024-07-03 08:58:14 +0200

    move searcher opener fab to filter overview

--- components/catalog/FilterOverview.tsx
+++ components/catalog/FilterOverview.tsx
@@ -1,43 +1,52 @@
-import { useSearchContext } from '@/app/katalog/context';
-import { PrimaryButton } from '@/theme/buttons/Buttons';
-import { getBooksByQuery } from '@/utils/fetchBooks';
-import { Box } from '@mui/material';
-import React from 'react'
-import CategoryChip from './CategoryChip';
+import { useSearchContext } from "@/app/katalog/context";
+import { PrimaryButton } from "@/theme/buttons/Buttons";
+import { getBooksByQuery } from "@/utils/fetchBooks";
+import { Box } from "@mui/material";
+import React from "react";
+import CategoryChip from "./CategoryChip";
+import SearcherOpenerFab from "./SearcheOpenerFab";
 interface FilterOverviewProps {
-    removeFilter: (key: string) => void;
-  }
-
-  const FilterOverview: React.FC<FilterOverviewProps> = ({ removeFilter }) => {
-    const {filters,setBooks} = useSearchContext();
-  return (
-    <Box className='w-full flex flex-row justify-between my-4'>
-
-        <Box className={'  flex-1'}>
-          {Object.entries(filters).map(([key, value], index) => (
-            <CategoryChip
-              key={index}
-              text={
-                value === "true" || value === "false" ? key : value.split(",").join(", ")
-              }
-              onRemove={() => removeFilter(key)}
-            />
-          ))}
-        </Box>
-
-          {Object.keys(filters).length > 0 && (
-          <PrimaryButton
-          className={'  flex-0'}
-            onClick={async () => {
-              const newBooks = await getBooksByQuery(filters);
-              setBooks(newBooks);
-            }}
-          >
-            Použít Filtry
-          </PrimaryButton>
-      )}
-      </Box>
-  )
+  removeFilter: (key: string) => void;
 }
 
-export default FilterOverview
+const FilterOverview: React.FC<FilterOverviewProps> = ({ removeFilter }) => {
+  const { filters, setBooks, isOpenSearcher, setOpenSearcher } =
+    useSearchContext();
+  return (
+    <Box className="w-full flex flex-row items-center justify-between my-4">
+      <SearcherOpenerFab
+        css={`flex-0 ${isOpenSearcher? "hidden" : "block"}`}
+        onClick={() => {
+          setOpenSearcher(!isOpenSearcher);
+        }}
+      />
+      <Box className={"  flex-1"}>
+        {Object.entries(filters).map(([key, value], index) => (
+          <CategoryChip
+            key={index}
+            text={
+              value === "true" || value === "false"
+                ? key
+                : value.split(",").join(", ")
+            }
+            onRemove={() => removeFilter(key)}
+          />
+        ))}
+      </Box>
+
+      {Object.keys(filters).length > 0 && (
+        <PrimaryButton
+          className={"  flex-0"}
+          onClick={async () => {
+            const newBooks = await getBooksByQuery(filters);
+            setBooks(newBooks);
+          }}
+        >
+          Použít Filtry
+        </PrimaryButton>
+      )}
+    </Box>
+  );
+};
+
+export default FilterOverview;
