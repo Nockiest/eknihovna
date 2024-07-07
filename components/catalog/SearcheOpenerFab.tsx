aebbde5c239d0623c98bd16753cd59ccb68f2a14
Andrew <ondralukes06@seznam.cz>
2024-07-07 12:05:00 +0200

    fixed type error

--- components/catalog/SearcheOpenerFab.tsx
+++ components/catalog/SearcheOpenerFab.tsx
@@ -1,31 +1,31 @@
-import { useSearchContext } from '@/app/katalog/context';
-import { Fab } from '@mui/material';
-import Image from 'next/image';
-import React from 'react'
+import { useSearchContext } from "@/app/katalog/context";
+import { Fab } from "@mui/material";
+import Image from "next/image";
+import React from "react";
 type SearcherOpenerFabProps = {
-    onClick?: () => void;
-    css?:string
-  };
+  onClick: () => void;
+  css?: string;
+};
 
-  const SearcherOpenerFab: React.FC<SearcherOpenerFabProps> = ({onClick, css}) => {
-
+const SearcherOpenerFab: React.FC<SearcherOpenerFabProps> = ({
+  onClick,
+  css,
+}) => {
   return (
     <Fab
-          size="large"
-          onClick={() => {
-            onClick()
-          }}
-          className={`z-1 ${css}  flex-0 flex justify-center items-center  `}
-        >
-          <Image
-            src="/icon/filter.svg"
-            alt="search"
-            width={32}
-            height={32}
-            className="m-1"
-          />
-        </Fab>
-  )
-}
+      size="large"
+      onClick={onClick}
+      className={`z-1 ${css}  flex-0 flex justify-center items-center  `}
+    >
+      <Image
+        src="/icon/filter.svg"
+        alt="search"
+        width={32}
+        height={32}
+        className="m-1"
+      />
+    </Fab>
+  );
+};
 
-export default SearcherOpenerFab
+export default SearcherOpenerFab;
