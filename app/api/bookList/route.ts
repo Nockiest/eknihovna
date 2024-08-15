7a152b58436e6d81ed4fcab0df4c2d25a203782d
Andrew <ondralukes06@seznam.cz>
2024-08-15 19:28:23 +0200

    x

--- app/api/bookList/route.ts
+++ app/api/bookList/route.ts
@@ -2,35 +2,37 @@
 // import prisma from '@/app/lib/prisma';
 // import { buildPrismaFilter } from '@/utils/buildPrismaFilter';
 
-import { NextRequest } from "next/server";
+import { Book } from "@/types/types";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-//   try {
-//     const { filters, page = 1, limit = 10 } = await req.json();
+  try {
+    const { filters, page = 1, limit = 10 } = await req.json();
 
-//     if (!filters) {
-//       return NextResponse.json({ error: "Server didn't receive filters" }, { status: 400 });
-//     }
+    if (!filters) {
+      return NextResponse.json({ error: "Server didn't receive filters" }, { status: 400 });
+    }
 
-//     if (page <= 0) {
-//       return NextResponse.json({ error: "Page number was set to 0 or less" }, { status: 400 });
-//     }
+    if (page <= 0) {
+      return NextResponse.json({ error: "Page number was set to 0 or less" }, { status: 400 });
+    }
 
 //     // Build the Prisma filter query
 //     const where = buildPrismaFilter(filters);
 
 //     // Fetch data using Prisma
-//     const books = await prisma.knihy.findMany({
-//       where,
-//       skip: (page - 1) * limit,
-//       take: limit,
-//       distinct: ['name'], // Select distinct on the 'name' field
-//     });
-//     return NextResponse.json(books);
-//   } catch (error) {
-//     console.error("Error fetching books:", error);
-//     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
-//   }
+    // const books = await prisma.knihy.findMany({
+    //   where,
+    //   skip: (page - 1) * limit,
+    //   take: limit,
+    //   distinct: ['name'], // Select distinct on the 'name' field
+    // });
+const books:Book[] = []
+    return NextResponse.json(books);
+  } catch (error) {
+    console.error("Error fetching books:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
 }
 
 // export default function GET(
