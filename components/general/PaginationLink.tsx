fa224be443acddc8111f4d94854b72bc295e3880
Andrew <ondralukes06@seznam.cz>
2024-08-19 10:35:59 +0200

    responsive paginationlinker

--- components/general/PaginationLink.tsx
+++ components/general/PaginationLink.tsx
@@ -5,13 +5,16 @@ interface PaginationLinkProps {
   pageNumber: number;
   page: number;
   folderName: string;
+  className?: string; // Optional prop for custom className
 }
 
-const PaginationLink: React.FC<PaginationLinkProps> = ({ pageNumber, page, folderName }) => {
+const PaginationLink: React.FC<PaginationLinkProps> = ({ pageNumber, page, folderName, className = '' }) => {
   const isActive = pageNumber === page;
-  const linkClassName = `px-3 py-1 rounded ${
+  const baseClassName = `px-3 py-1 rounded ${
     isActive ? "bg-primary-400 text-text-950" : "bg-secondary-900 text-text-100"
   }`;
+
+  const linkClassName = `${baseClassName} ${className}`.trim(); // Combine base and custom className
 
   return (
     <Link href={`/${folderName}?page=${pageNumber}`} className={linkClassName}>
