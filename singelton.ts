7e9e3c62105b0b1a7aa82de3bf343f67d3f646bd
Andrew <ondralukes06@seznam.cz>
2024-08-19 10:28:01 +0200

    turnde off tests2

--- singelton.ts
+++ singelton.ts
@@ -1,16 +1,16 @@
-import { PrismaClient } from '@prisma/client'
-import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended'
-import { prisma } from './lib/prisma'
+// import { PrismaClient } from '@prisma/client'
+// import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended'
+// import { prisma } from './lib/prisma'
 
 
 
-jest.mock('./client', () => ({
-  __esModule: true,
-  default: mockDeep<PrismaClient>(),
-}))
+// jest.mock('./client', () => ({
+//   __esModule: true,
+//   default: mockDeep<PrismaClient>(),
+// }))
 
-beforeEach(() => {
-  mockReset(prismaMock)
-})
+// beforeEach(() => {
+//   mockReset(prismaMock)
+// })
 
-export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>
+// export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>
