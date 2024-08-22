4a395132051d6f34392756b4aeae46166ff4f100
Andrew <ondralukes06@seznam.cz>
2024-08-22 09:52:21 +0200

    pagination linker2

--- signals/appState.tsx
+++ signals/appState.tsx
@@ -1,11 +1,11 @@
-import { signal } from "@preact/signals";
+// import { signal } from "@preact/signals";
 
-export default function createAppState() {
-    // const todos = signal([]);
-    const NavBarOpen = signal(false);
-    // const completed = computed(() => {
-    //   return todos.value.filter(todo => todo.completed).length
-    // });
+// export default function createAppState() {
+//     // const todos = signal([]);
+//     const NavBarOpen = signal(false);
+//     // const completed = computed(() => {
+//     //   return todos.value.filter(todo => todo.completed).length
+//     // });
 
-    return { NavBarOpen }
-  }
+//     return { NavBarOpen }
+//   }
