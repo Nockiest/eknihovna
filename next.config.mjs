584d9d38056e409f9812fee8c22b54ee1842ea1a
Andrew <ondralukes06@seznam.cz>
2024-08-23 10:42:28 +0200

    fixed auth redirect error

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'https://eknihovna.vercel.app/api/:path*', // Proxy to the production API
-        },
-      ];
-    },
+   
   };
 
 export default nextConfig;
