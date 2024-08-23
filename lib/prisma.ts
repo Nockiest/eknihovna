00ffd6cdedf8ab7d71b0c02caefaa1884cc53bf3
Andrew <ondralukes06@seznam.cz>
2024-08-23 16:57:01 +0200

    force create new id on upload

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -9,5 +9,4 @@ export const prisma =
     log: ['query'],
   })
 
-if (process.env.NODE_ENV !== 'production')
-globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
