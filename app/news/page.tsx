a9965812a118ee1118b6b6c6b401cf0b0c55e297
Andrew <ondralukes06@seznam.cz>
2024-08-25 13:23:32 +0200

    fixing metadata

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,33 +1,19 @@
-'use client'
-import React, { useEffect, useState } from 'react';
 import { Container } from '@mui/material';
 import Message from '@/components/general/Message';
-import axios from 'axios';
 import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: "Novinky v G.O. knihovně",
   description: "Seznam novinek",
 };
+
+type Message = {
+  message: string;
+  date: string;
+}
 const NewsPage: React.FC = () => {
-  const [messages, setMessages] = useState([
-    { message: "Vítáme vás v první verzi stránky. Můžete si prohlédnout aktuální stav knihovního katalogu, nebo zjistit užitečné informace o naší knihovně.", date: "2023-06-24" }
-  ]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`${process.env.NEXT_PUBLIC_APP_API_URL}/test`);
-        console.log(response.data);
-        // Update messages state if needed based on the API response
-        // setMessages(response.data.books);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
+  const  messages:Message[] = [
+   {message:"Vítáme vás v první verzi stránky. Můžete si prohlédnout aktuální stav knihovního katalogu, nebo zjistit užitečné informace o naší knihovně.", date: "2023-06-24" }
+  ]
   return (
     <Container maxWidth="sm">
       {messages.map((msg, index) => (
@@ -38,26 +24,3 @@ const NewsPage: React.FC = () => {
 };
 
 export default NewsPage;
-
-// const books =  await prisma.knihy.findMany()
-  // console.log(books)
-  // useEffect(() => {
-  //   const findBooks = async ()=> {
-
-  //   }
-  //   findBooks()
-  // }, [])
-
-    // useEffect(() => {
-  //   const fetchMessages = async () => {
-  //     try {
-  //       console.log(response)
-  //       // setMessages(response.data);
-  //     } catch (err: any) {
-  //       // setError('Failed to fetch messages');
-  //       console.error(err.message);
-  //     }
-  //   };
-
-  //   fetchMessages();
-  // }, []);
