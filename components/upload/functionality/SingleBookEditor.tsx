72d4e55f495de8cef9f0bcac9b8de9bf643f15d7
Andrew <ondralukes06@seznam.cz>
2024-09-12 15:06:54 +0200

    fix ts

--- components/upload/functionality/SingleBookEditor.tsx
+++ components/upload/functionality/SingleBookEditor.tsx
@@ -1,24 +1,24 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { Book, UploadJsonData} from "@/types/types";
+import { Book, UploadJsonData } from "@/types/types";
 import { fetchFilteredBooks } from "@/utils/apiConections/fetchFilteredBooks";
 import { defaultFilters } from "@/data/values";
 import { postDataToEndpoint } from "@/utils/apiConections/postDataToUpload";
 import { PrimaryButton } from "@/theme/buttons/Buttons";
 import BookEditForm from "@/components/general/BookEditForm";
 import { Box } from "@mui/material";
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from "uuid";
 
 const newBook: Book = {
   id: uuidv4(),
   book_code: -1,
-  name: '',
-  author: '',
-  category: '',
+  name: "",
+  author: "",
+  category: "",
   genres: [],
-  umisteni: '',
-  signatura: '',
-  zpusob_ziskani: '',
+  umisteni: "",
+  signatura: "",
+  zpusob_ziskani: "",
   formaturita: false,
   available: false,
   rating: -1,
@@ -28,20 +28,20 @@ const SingleBookEditor = ({
 }: {
   setResponseMessage: React.Dispatch<React.SetStateAction<string | null>>;
 }) => {
-  const [bookId, setBookId] = useState('');
+  const [bookId, setBookId] = useState("");
   const [book, setBook] = useState<Book | null>(null);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   // Fetch book details based on the entered ID
   const fetchBook = async () => {
     if (!bookId) {
-      setError('Please enter a valid book ID');
+      setError("Please enter a valid book ID");
       return;
     }
 
     setLoading(true);
-    setError('');
+    setError("");
     try {
       const response = await fetchFilteredBooks(
         defaultFilters, // defaultFilters or appropriate filter object
@@ -53,11 +53,11 @@ const SingleBookEditor = ({
       if (response.length > 0) {
         setBook(response[0]);
       } else {
-        setError('Book not found');
+        setError("Book not found");
       }
     } catch (err) {
-      console.error('Error fetching book:', err);
-      setError('Book not found or an error occurred');
+      console.error("Error fetching book:", err);
+      setError("Book not found or an error occurred");
     } finally {
       setLoading(false);
     }
@@ -69,19 +69,17 @@ const SingleBookEditor = ({
     let newValue: string | string[] | boolean | number = value;
 
     // Adjust value based on input type
-    if (type === 'checkbox') {
+    if (type === "checkbox") {
       newValue = checked;
-    } else if (name === 'genres') {
-      newValue = value.split(',').map((v) => v.trim());
-    } else if (type === 'number') {
+    } else if (name === "genres") {
+      newValue = value.split(",").map((v) => v.trim());
+    } else if (type === "number") {
       newValue = value ? parseInt(value, 10) : -1; // Handling numeric input, default to -1 if empty
     }
 
     if (!book) {
       // If book is null, initialize a new book object
-      setBook({
-        newBook
-      });
+      setBook(newBook);
     } else {
       // Update existing book object
       setBook((prevBook) => {
@@ -99,30 +97,30 @@ const SingleBookEditor = ({
     if (!book) return;
 
     setLoading(true);
-    setError('');
+    setError("");
     try {
       const newEntry: UploadJsonData = {
         headers: [
-          'id',
-          'book_code',
-          'name',
-          'author',
-          'category',
-          'genres',
-          'umisteni',
-          'signatura',
-          'zpusob_ziskani',
-          'formaturita',
-          'available',
-          'rating',
+          "id",
+          "book_code",
+          "name",
+          "author",
+          "category",
+          "genres",
+          "umisteni",
+          "signatura",
+          "zpusob_ziskani",
+          "formaturita",
+          "available",
+          "rating",
         ],
         rows: [book],
       };
       await postDataToEndpoint(newEntry);
-      setResponseMessage('Book updated successfully!');
+      setResponseMessage("Book updated successfully!");
     } catch (err) {
-      console.error('Error updating book:', err);
-      setError('Failed to update the book');
+      console.error("Error updating book:", err);
+      setError("Failed to update the book");
     } finally {
       setLoading(false);
     }
@@ -130,9 +128,8 @@ const SingleBookEditor = ({
 
   // Initialize a new book with a new UUID and empty values
   const createNewBook = () => {
-
     setBook(newBook);
-    setBookId(''); // Clear the book ID input field
+    setBookId(""); // Clear the book ID input field
   };
 
   return (
@@ -153,11 +150,11 @@ const SingleBookEditor = ({
 
       {loading && <p>Načítání...</p>}
 
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
 
       {book && (
         <div>
-          <h3>{bookId ? 'Upravit Knihu' : 'Vytvořit Novou Knihu'}</h3>
+          <h3>{bookId ? "Upravit Knihu" : "Vytvořit Novou Knihu"}</h3>
           <BookEditForm
             book={book}
             handleInputChange={handleInputChange}
