41ad72851f72f391316b923c035ddd8e53258fc3
Andrew <ondralukes06@seznam.cz>
2024-10-16 19:27:23 +0200

    remove env.production from github

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -55,7 +55,7 @@ export const findManyPrismaUniquePrismaBooksColumn = async (
     distinct: [columnName as keyof UniqueBookValue],
   });
 };
-
+//
 export const findUniquePrismaBooks = async (
   id: string,
   ctx: Context = context
