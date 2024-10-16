b6adfee91b48d5950f9f9f169d80c8c7fd5287f1
Andrew <ondralukes06@seznam.cz>
2024-10-16 19:21:35 +0200

    clensing jest and .env from my repo

--- __tests__/inprogress/SingleDelete.test.jsx
+++
@@ -1,3 +0,0 @@
-// test it deletes single book base on id
-
-// test it diesnt delete book without providing valid id
--- __tests__/inprogress/SingleUpload.test.jsx
+++
@@ -1,3 +0,0 @@
-// test it uploads single book
-
-// test it reqrites book with the same id
