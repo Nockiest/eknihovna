568b1b94d80deb3a60dfc793e3a353875c787d88
Andrew <ondralukes06@seznam.cz>
2024-10-19 11:45:05 +0200

    added credit on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,43 @@
-'use client'
+"use client";
 import OpeningHours from "@/components/general/OpeningHours";
 import { Container, Typography, Button, Box } from "@mui/material";
 import Link from "next/link";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
-export default function Home( ) {
-
-
+export default function Home() {
   return (
     <Container maxWidth={"lg"}>
       <Typography variant="body1">
         Vítejte na domovské stránce knihovny Gymnázia Opatov, kde se můžete
         seznámit s bohatým fondem naší školní knihovny a získat důležité
-        informace. Naše knihovna je otevřeným prostorem pro vaše vzdělání a
-        inspiraci. Nabízíme široký výběr literatury, studijních materiálů a
-        časopisů. Projděte si naši sbírku a objevujte nové příběhy, poznatky a
-        světy. Jsme tu pro vás, abychom vám pomohli s vašimi studijními
-        potřebami a abychom vám poskytli prostor k odpočinku a pohodlnému čtení.
+        informace.
+      </Typography>
+      <br />
+      <Typography variant="body1">
+        Naše knihovna je otevřeným prostorem pro vaše vzdělání a inspiraci.
+        Nabízíme široký výběr literatury, studijních materiálů a časopisů.
+      </Typography>
+      <br />
+      <Typography variant="body1">
+        Projděte si naši sbírku a objevujte nové příběhy, poznatky a světy. Jsme
+        tu pro vás, abychom vám pomohli s vašimi studijními potřebami a abychom
+        vám poskytli prostor k odpočinku a pohodlnému čtení.
+      </Typography>
+      <br />
+      <Typography variant="body1">
         Pokud máte zájem o konkrétní knihu, nebo potřebujete poradit s
         vyhledáním informací, neváhejte se po školních hodinách obrátit na
-        profesorku Bauerovou. Náš cíl je podporovat váš akademický růst a
-        zvědavost. Přijďte navštívit naši knihovnu a objevte, co všechno pro vás
-        máme připraveného. Těšíme se na vaši návštěvu!
+        profesorku Bauerovou.
       </Typography>
+      <br />
+      <Typography variant="body1">
+        Náš cíl je poskytnout vám kvalitní světovou i domácí literaturu a pomoct vám sehnat maturitní
+        četbu. Přijďte navštívit
+        naši knihovnu a objevte, co všechno pro vás máme připraveného. Těšíme se
+        na vaši návštěvu!
+      </Typography>
+      <br />
+
       <OpeningHours />
       <Box mt={2} display="flex" justifyContent="center">
         <Button
@@ -35,9 +50,12 @@ export default function Home( ) {
           </Link>
         </Button>
       </Box>
-
+      <Container className=" text-secondary-950  bg-primary-600 p-4 border border-gray-300 rounded-md mb-4">
+      <Typography variant="body1">
+        Stránku vytvořil Ondřej Lukeš. Děkuji za pomoc všem co se podíleli na projektu: Patriku
+        Holbovi, Davidu Laušmanovi, prof. Bauerové a redakci Peřinky.
+      </Typography>
+      </Container>
     </Container>
   );
 }
-
- 
