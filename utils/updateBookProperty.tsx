9fa832e5e1debb5b8124cfd09da049ccadb5a7c4
Andrew <ondralukes06@seznam.cz>
2024-10-26 09:48:46 +0200

    improved useCurrentBreakpoint utils fce

--- utils/updateBookProperty.tsx
+++ utils/updateBookProperty.tsx
@@ -1,6 +1,17 @@
 import { emptyBook } from "@/data/values";
 import { Book } from "@/types/types";
 import {v4 as uuidv4} from 'uuid'
+ 
+/**
+ * Updates a specific property of a book based on the provided parameters.
+ * If the book is null, creates a new book with a unique ID.
+ * @param name - The name of the property to update.
+ * @param value - The new value for the property.
+ * @param type - The type of the property.
+ * @param checked - Indicates if a checkbox property is checked.
+ * @param book - The book object to update or null if creating a new book.
+ * @returns The updated book object with the specified property modified.
+ */
 const updateBookProperty = (name: string, value: string, type: string, checked: boolean, book: Book | null): Book => {
     let newValue: string | string[] | boolean | number = value;
 
