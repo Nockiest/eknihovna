e99020c585f79b63085f84f8f4f2f6e02671d6b4
Andrew <ondralukes06@seznam.cz>
2024-11-21 18:25:16 +0100

    add meta data to all pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 import OpeningHours from "@/components/OpeningHours";
 import { Container, Typography, Button, Box } from "@mui/material";
 import Link from "next/link";
@@ -6,52 +6,48 @@ import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import OtherProjectLinker from "@/components/OtherProjectLinker";
 import useCurrentBreakpoint from "@/features/hooks/useCustomBreakpoint";
 import React from "react";
-
-
+import GOMobileheader from "@/components/GOMobileheader";
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Novinky v GO eknihovně",
+  description: "GO eknihovna hlavní stránka",
+};
 export default function Home() {
-  const size = useCurrentBreakpoint();
   return (
     <Container maxWidth={"lg"} className="flex flex-col gap-2">
-      {(size === "xs" || size === "sm")  &&
 
-        <Typography
-          variant="h1"
-          className="  text-center  "
-          align="center"
-        >
-          ŠKOLNÍ KNIHOVNA GO
+      <GOMobileheader />
+
+      <Container>
+        <Typography variant="body1">
+          Vítejte na domovské stránce knihovny Gymnázia Opatov, kde se můžete
+          seznámit s bohatým fondem naší školní knihovny a získat důležité
+          informace.
         </Typography>
-         }
-
-
-     <Container >
-     <Typography variant="body1">
-        Vítejte na domovské stránce knihovny Gymnázia Opatov, kde se můžete
-        seznámit s bohatým fondem naší školní knihovny a získat důležité
-        informace.
-      </Typography>
-      <br />
-      <Typography variant="body1">
-        Naše knihovna je otevřeným prostorem pro vaše vzdělání a inspiraci.
-        Nabízíme široký výběr literatury, studijních materiálů a časopisů.
-      </Typography>
-      <br />
-      <Typography variant="body1">
-        Projděte si naši sbírku a objevujte nové příběhy, poznatky a světy. Jsme
-        tu pro vás, abychom vám pomohli s vašimi studijními potřebami a abychom
-        vám poskytli prostor k odpočinku a pohodlnému čtení.
-      </Typography>
-      <br />
-      <Typography variant="body1">
-        Pokud máte zájem o konkrétní knihu, nebo potřebujete poradit s  yhledáním informací, neváhejte se po školních hodinách obrátit na         profesorku Bauerovou.
-      </Typography>
-      <br />
-      <Typography variant="body1">
-        Náš cíl je poskytnout vám kvalitní světovou i domácí literaturu. Přijďte
-        navštívit naši knihovnu a objevte, co všechno pro vás máme připraveného.
-        Těšíme se na vaši návštěvu!
-      </Typography>
-     </Container>
+        <br />
+        <Typography variant="body1">
+          Naše knihovna je otevřeným prostorem pro vaše vzdělání a inspiraci.
+          Nabízíme široký výběr literatury, studijních materiálů a časopisů.
+        </Typography>
+        <br />
+        <Typography variant="body1">
+          Projděte si naši sbírku a objevujte nové příběhy, poznatky a světy.
+          Jsme tu pro vás, abychom vám pomohli s vašimi studijními potřebami
+          a abychom vám poskytli prostor k odpočinku a pohodlnému čtení.
+        </Typography>
+        <br />
+        <Typography variant="body1">
+          Pokud máte zájem o konkrétní knihu, nebo potřebujete poradit
+          s  yhledáním informací, neváhejte se po školních hodinách obrátit na 
+          profesorku Bauerovou.
+        </Typography>
+        <br />
+        <Typography variant="body1">
+          Náš cíl je poskytnout vám kvalitní světovou i domácí literaturu.
+          Přijďte navštívit naši knihovnu a objevte, co všechno pro vás máme
+          připraveného. Těšíme se na vaši návštěvu!
+        </Typography>
+      </Container>
 
       <Box mt={2} display="flex" justifyContent="center">
         <Button
@@ -66,8 +62,9 @@ export default function Home() {
       </Box>
       <Container className="mx-auto mt-2 text-secondary-950  bg-primary-600 p-4 border border-gray-300 rounded-md mb-4">
         <Typography variant="body1">
-          Stránku vytvořil Ondřej Lukeš. Děkuji za pomoc všem co se podíleli na projektu: Patriku Holbovi, Davidu Laušmanovi, prof. Bauerové a redakci
-          Peřinky.
+          Stránku vytvořil Ondřej Lukeš. Děkuji za pomoc všem co se podíleli
+          na projektu: Patriku Holbovi, Davidu Laušmanovi, prof. Bauerové
+          a redakci Peřinky.
         </Typography>
       </Container>
       <OpeningHours />
---
+++ components/GOMobileheader.tsx
@@ -0,0 +1,19 @@
+'use client'
+import useCurrentBreakpoint from "@/features/hooks/useCustomBreakpoint";
+import { Typography } from "@mui/material";
+import React from "react";
+
+const GOMobileheader = () => {
+  const size = useCurrentBreakpoint();
+  return (
+    <>
+      {(size === "xs" || size === "sm") && (
+        <Typography variant="h1" className="  text-center  " align="center">
+          ŠKOLNÍ KNIHOVNA GO
+        </Typography>
+      )}
+    </>
+  );
+};
+
+export default GOMobileheader;
