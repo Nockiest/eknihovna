2ac5f22c94da17ce1b7b3bcbb978029a8abeac1a
Andrew <ondralukes06@seznam.cz>
2024-11-21 21:17:25 +0100

    added xml file

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,6 @@ export default function RootLayout({
             `,
         }}
       />
-
       <Head>
         {/* Primary Favicon */}
         <link rel="icon" type="image/x-icon" href="/icon/GO.ico" />
