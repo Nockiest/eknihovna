3f5936a6022c92016d18c7f9d441640370260f6b
Andrew <ondralukes06@seznam.cz>
2024-11-21 20:48:58 +0100

    added google analytics tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import Head from "next/head";
 import BugReportSection from "@/features/sections/BugReport";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import Script from "next/script";
 // import ReactQueryProvider from "@/utils/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,11 +15,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "GO eknihovna",
   description: "Školní knihovna Gymnázia Opatov",
-  applicationName: 'GO eknihovna',
-  keywords: ['knihovna', 'eknihovna', 'gymnázium', "opatov", "GO"],
+  applicationName: "GO eknihovna",
+  keywords: ["knihovna", "eknihovna", "gymnázium", "opatov", "GO"],
   creator: "Ondřej Lukeš",
   publisher: "Ondřej Lukeš",
-  icons: "/img/loga/bookLogo.png"
+  icons: "/img/loga/bookLogo.png",
 };
 
 export default function RootLayout({
@@ -28,6 +29,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="cz">
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NWF34JRK');
+            `,
+        }}
+      />
+
       <Head>
         {/* Primary Favicon */}
         <link rel="icon" type="image/x-icon" href="/icon/GO.ico" />
@@ -42,12 +57,22 @@ export default function RootLayout({
         {/* Optional: Manifest File */}
         <link rel="manifest" href="/icon/GO-manifest.json" />
 
-        <meta name="google-site-verification" content="hjQ9ZFX153H0STRp1SFSLiakzbOMsi_S6s4jPvNKKJA" />
+        <meta
+          name="google-site-verification"
+          content="hjQ9ZFX153H0STRp1SFSLiakzbOMsi_S6s4jPvNKKJA"
+        />
         {/* Optional: Theme Color */}
         <meta name="theme-color" content="#413330" />
       </Head>
       <body className={inter.className}>
-        {/* <ReactQueryProvider   > */}
+      <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NWF34JRK"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <ThemeProvider theme={theme}>
           <Hero />
           <main className="flex min-h-screen flex-col items-center justify-between pt-12">
