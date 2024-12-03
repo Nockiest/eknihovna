3dfb39c0c209d5652f01a68b1c1c8bf93b4fa4b8
Andrew <ondralukes06@seznam.cz>
2024-12-03 18:59:13 +0100

    changed one description

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import { Metadata } from "next";
 // import { GoogleAnalytics } from "nextjs-google-analytics"; xx
 export const metadata: Metadata = {
   title: "GO eknihovna",
-  description: "GO eknihovna hlavní stránka",
+  description: "Gymnazium Opatov eknihovna hlavní stránka",
   applicationName: "GO eknihovna",
   keywords: ["knihovna", "eknihovna","gymnázium opatov", "gymnázium", "opatov", "GO"],
   creator: "Ondřej Lukeš",
