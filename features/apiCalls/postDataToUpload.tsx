8b170978f3dbc5dc8804f5a65abe43a95213f347
Andrew <ondralukes06@seznam.cz>
2024-12-05 19:55:12 +0100

    extracted error hanlding to its own fce

--- features/apiCalls/postDataToUpload.tsx
+++ features/apiCalls/postDataToUpload.tsx
@@ -22,22 +22,25 @@ export const postDataToUpload = async (
     return response;
   } catch (error: any) {
     if (axios.isAxiosError(error)) {
-      if (error.response) {
-        // Handle specific status codes
-        if (error.response.status >= 400 && error.response.status < 500) {
-          console.error("Client error:", error.response.data.message);
-          alert(`Chyba na straně klienta: ${error.response.data.message}`);
-        } else if (error.response.status >= 500) {
-          console.error("Server error:", error.response.data.message);
-          alert(`Chyba na straně serveru: ${error.response.data.message}`);
-        }
-      } else {
-        console.error("No response received:", error.message);
-        alert(`Žádná odpověď nebyla přijata: ${error.message}`);
-      }
+      handleError(error?.response?.status, error?.response?.data.message)
     } else {
       console.error("An unexpected error occurred:", error);
       alert(`Došlo k neočekávané chybě: ${error}`);
     }
   }
 };
+function handleError(statusCode: number| undefined, errorMessage: string): void {
+  if (!statusCode){
+    console.error("Unexpected status code:", statusCode);
+    alert(`Neočekávaný stavový kód: ${statusCode}`);
+    return
+  }
+
+  if (statusCode >= 400 && statusCode < 500) {
+    console.error("Client error:", errorMessage);
+    alert(`Chyba na straně klienta: ${errorMessage}`);
+  } else if (statusCode >= 500) {
+    console.error("Server error:", errorMessage);
+    alert(`Chyba na straně serveru: ${errorMessage}`);
+  }
+}
