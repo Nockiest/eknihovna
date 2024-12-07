d39f631b084a7108e6bd865fe224546e904c2bbb
Andrew <ondralukes06@seznam.cz>
2024-12-07 10:23:31 +0100

    improved pagination linker functioning

--- components/PaginationLinker.tsx
+++ components/PaginationLinker.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import Link from "next/link";
+// import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { Box } from "@mui/material";
 import PaginationLink from "./PaginationLink";
-import useCurrentBreakpoint from "@/features/hooks/useCustomBreakpoint";
+// import useCurrentBreakpoint from "@/features/hooks/useCustomBreakpoint";
 
 interface PaginationProps {
   totalEntries: number;
@@ -28,24 +28,57 @@ const Pagination: React.FC<PaginationProps> = ({
   useEffect(() => {
     setTotalPages(Math.ceil(totalEntries / itemsPerPage));
   }, [totalEntries, itemsPerPage]);
-  const breakPoint = useCurrentBreakpoint();
-  const [offset, setOffset] = useState<number>(5);
-  useEffect(() => {
-    if (breakPoint && ["xs", "sm"].indexOf(breakPoint) >= 0) {
-      setOffset(5);
-    } else {
-      setOffset(10);
-    }
-  }, [breakPoint]);
+  // const breakPoint = useCurrentBreakpoint();
+  const offset = 3
+  // const [offset, setOffset] = useState<number>(5);
+  // useEffect(() => {
+  //   if (breakPoint && ["xs", "sm"].indexOf(breakPoint) >= 0) {
+  //     setOffset(5);
+  //   } else {
+  //     setOffset(10);
+  //   }
+  // }, [breakPoint]);
 
-  const getVisiblePageNumbers = () => {
-    const startPage = Math.max(1, page - offset);
-    const endPage = Math.min(totalPages, page + offset);
+  const getVisiblePageNumbers = ():number[] => {
+    // let pagesFront =  Math.min( page- 1   , offset)
+    // let pagesBack = Math.min(totalPages - page , offset)
     const pageNumbers = [];
-    for (let i = startPage; i <= endPage; i++) {
-      pageNumbers.push(i);
+    let unfoundPages = 0
+    // try to get 3 pages in front of the current
+    for (let i = page - offset; i <= page + offset; i++) {
+       // add found pages to visible
+      if (i >= 1 && i <= totalPages) {
+        console.log(i)
+       pageNumbers.push(i);
+      } else {
+        unfoundPages += 1
+      }
     }
-    return pageNumbers;
+    if (unfoundPages === 0) {
+      return pageNumbers;
+
+    } else if (page - offset > 0) {
+      for (let i = page - offset; i > 0 && unfoundPages>0; i-- ){
+        pageNumbers.push(i);
+        unfoundPages--
+      }
+    } else if (page + offset < totalPages) {
+      for (let i = page + offset +1; i < totalPages && unfoundPages>0; i++ ){
+        console.log(i)
+        pageNumbers.push(i);
+        unfoundPages--
+      }
+    }
+    return pageNumbers.sort((a, b) => a - b);;
+    // any unfoun number of pages in front will be added to the page looked for in the back
+    // look for pages in the back
+
+
+    // const startPage = Math.max(1, page - offset);
+    // const endPage = Math.min(totalPages, page + offset);
+    // for (let i = startPage; i <= endPage; i++) {
+    //   pageNumbers.push(i);
+    // }
   };
 
   const visiblePageNumbers = getVisiblePageNumbers();
@@ -68,14 +101,14 @@ const Pagination: React.FC<PaginationProps> = ({
       ) : (
         <>
           {/* Show '...' before the first page link if necessary */}
-          {page - 1 > offset && (
+          {/* {page - 1 > offset && (
             <PaginationLink
               pageNumber={1}
               currentPage={page}
               folderName={folderName}
               className="px-3 py-1 rounded bg-gray-200 text-text-100"
             />
-          )}
+          )} */}
 
           {/* Render visible page numbers */}
           {visiblePageNumbers.map((pageNumber, key) => (
@@ -87,7 +120,7 @@ const Pagination: React.FC<PaginationProps> = ({
             />
           ))}
 
-          {/* Show '...' after the last page link if necessary */}
+          {/* Show '...' after the last page link if necessary
           {page < totalPages - offset && (
             <PaginationLink
               pageNumber={totalPages}
@@ -95,7 +128,7 @@ const Pagination: React.FC<PaginationProps> = ({
               folderName={folderName}
               className="px-3 py-1 rounded bg-gray-200 text-text-100"
             />
-          )}
+          )} */}
         </>
       )}
     </Box>
