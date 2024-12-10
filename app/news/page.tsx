60d3b5f54f7efae857f0c7cb96108cfaaba5769f
Andrew <ondralukes06@seznam.cz>
2024-12-10 16:36:22 +0100

    fixedtypos

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -19,9 +19,9 @@ type Message = {
 const NewsPage: React.FC = () => {
   const messages: Message[] = [
     {
-      message:
-        "Eknihovna je tu! Prozkouměj tisíce titulů našehi katalogu, informuj se o jejich aktuálním stavu a pujč si je ještě dnes!! Případné návrhy a prosby směřujte buď přes dotazník níže, nebo přímo na email ondluk.m@zaci.gopat.cz ",
-      date: "2024-12-10",
+     message:
+  "Eknihovna je tu! Prozkoumej tisíce titulů našeho katalogu, informuj se o jejich aktuálním stavu a půjč si je ještě dnes! Případné návrhy a prosby směřujte buď přes tlačítko níže na stránce, nebo přímo na email ondluk.m@zaci.gopat.cz.",
+date: "2024-12-10",
     },
   ];
   return (
