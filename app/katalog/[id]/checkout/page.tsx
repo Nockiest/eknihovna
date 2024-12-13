e0e83dfa9de3440627142b70e3098f80a856ea7b
Andrew <ondralukes06@seznam.cz>
2024-12-13 17:46:41 +0100

    trying differnet barcode methods

--- app/katalog/[id]/checkout/page.tsx
+++ app/katalog/[id]/checkout/page.tsx
@@ -1,3 +1,38 @@
+// 'use client'
+// import React, { useState } from 'react';
+// import { QrReader } from 'react-qr-reader';
+
+
+// interface TestProps {
+//   // Define any props if needed
+// }
+
+// const Page: React.FC<TestProps> = (props) => {
+//   const [data, setData] = useState<string>('No result');
+
+//   return (
+//     <>
+//       <QrReader
+//         onResult={(result: any, error: Error | null|undefined) => {
+//           if (result) {
+//             setData(result?.text);
+//           }
+
+//           if (error) {
+//             console.info(error);
+//           }
+//         }}
+//         scanDelay={2000}
+//         constraints={{facingMode: 'user'}}
+//         // style={{ width: '100%' }}
+//       />
+//       <p>{data}</p>
+//     </>
+//   );
+// };
+
+// export default Page;
+
 'use client'
 import { PrimaryButton } from '@/theme/buttons/Buttons';
 import { Box, Typography } from '@mui/material';
@@ -5,6 +40,7 @@ import  { useState } from 'react';
 import { Result } from '@zxing/library';
 
 import BarcodeScannerComponent from 'react-qr-barcode-scanner';
+// import BarcodeScanner from '@/components/barcode/BarcodeScanner';
 // type Result = {
 //     text: string;
 //   };
@@ -17,43 +53,64 @@ const Page = () => {
   const [scanning, setScanning] = useState(false);
   const [barcodeData, setBarcodeData] = useState('');
 
-  const handleScan = (error: unknown, result?: Result | undefined): void => {
-    if (result) {
-      const supportedFormats = ['EAN_13', 'QR_CODE', 'CODE_128'];
-      if (supportedFormats.includes(result.getBarcodeFormat())) {
-        setBarcodeData(result.getText());
-        setScanning(false);
-      } else {
-        console.warn('Unsupported barcode format:', result.getBarcodeFormat());
-      }
-    } else if (error) {
-      console.error('Error scanning barcode:', error);
-    }
-  };
+//   const handleScan = (error: unknown, result?: Result | undefined): void => {
+//     if (result) {
+//       const supportedFormats = ['EAN_13', 'QR_CODE', 'CODE_128'];
+//       if (supportedFormats.includes(result.getBarcodeFormat())) {
+//         setBarcodeData(result.getText());
+//         setScanning(false);
+//       } else {
+//         console.warn('Unsupported barcode format:', result.getBarcodeFormat());
+//       }
+//     } else if (error) {
+//       console.error('Error scanning barcode:', error);
+//     }
+//   };
 
   return (
     <Box>
+
       <Typography variant='h1'>Checkout Page</Typography>
       <PrimaryButton onClick={() => setScanning(!scanning)}>
         {scanning ? 'Přestat nahrávat' : 'Nahrát čárový kód'}
       </PrimaryButton>
-      {scanning && (
-       <BarcodeScannerComponent
-       width={500}
-       height={500}
-       onUpdate={(error, result) => handleScan(error, result)}
-    //    options={{
-    //      formatsToSupport: [
-    //        'ean_13',
-    //        'qr_code',
-    //        'code_128',
-    //        'code_39',
-    //        'upc_a',
-    //      ], // Add additional formats as needed
-    //    }}
-     />
+      {/* {scanning && (
+    //    <BarcodeScanner
+    //    scanning={scanning}
+    //     setScanning={setScanning}
+    //    />
+      )} */}
 
 
+
+    {scanning && (
+         <BarcodeScannerComponent
+        //  width={500}
+        //  height={500}
+         onUpdate={(err, result) => {
+            console.log(err,result)
+           if (result) setBarcodeData(result.getText());
+           else setBarcodeData("Not Found");
+         }}
+         onError={ (error:any) =>
+            console.error(error)
+         }
+         torch={true}
+       />
+    //     <BarcodeScannerComponent
+    //       width={500}
+    //       height={500}
+    //       onUpdate={handleScan}
+    //       // options={{
+    //       //   formatsToSupport: [
+    //       //     'ean_13',
+    //       //     'qr_code',
+    //       //     'code_128',
+    //       //     'code_39',
+    //       //     'upc_a',
+    //       //   ],
+    //       // }}
+    //     />
       )}
       {barcodeData && <p>Scanned Barcode: {barcodeData}</p>}
     </Box>
